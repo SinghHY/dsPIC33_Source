7cc8474d7a91e35e3276621de63fe61776234209
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2019-01-09 12:12:11 -0500

    update

--- CCS_Source.X/Main.c
+++ CCS_Source.X/Main.c
@@ -9,13 +9,14 @@
 #include <33FJ256GP510.h>
 #fuses XT,NOWDT,NOPROTECT
 #device ADC = 12 
-#use delay(clock = 100Mhz, crystal = 8Mhz)
+#use delay(clock = 100Mhz, crystal = 40Mhz)
 #use spi(SLAVE, SPI2, BITS = 8, MODE = 1, ENABLE = PIN_G9, stream = SPI_2)
 
-#define GREEN_LED  PIN_A6
-#define RED_LED  PIN_A7
+#define Probe_Pin  PIN_E7
 
-int8 i, Error;
+
+int8 i, ProbeId = 0;
+int probe, Voltage, Temp;
    
 #bank_dma 
     unsigned int8  TxBuffer[4];
@@ -27,10 +28,20 @@ void main()
     
    
    output_float(PIN_G9); // SS as an input
-
-
-   for(i = 0; i <= 3; i++)
-       TxBuffer[i] = i * 10;    
+   setup_adc_ports(sAN0 | sAN2 | sAN30, VSS_VDD);
+   setup_adc(ADC_CLOCK_INTERNAL);
+   
+   
+   setup_timer2(TMR_INTERNAL | TMR_DIV_BY_64, 500);
+   setup_compare(1, COMPARE_PWM | COMPARE_TIMER2);
+   set_pwm_duty(1,0);
+   
+   setup_compare(2, COMPARE_PWM | COMPARE_TIMER2);
+   set_pwm_duty(2,0);
+   
+   setup_compare(3, COMPARE_PWM | COMPARE_TIMER2);
+   set_pwm_duty(3,0);
+   
    
    setup_dma(0, DMA_IN_SPI2, DMA_BYTE);   
    dma_start(0, DMA_CONTINOUS ,&RxBuffer[0],3); 
@@ -40,11 +51,24 @@ void main()
 
    while(1)
    {
-       output_high(GREEN_LED);
-       delay_ms(500);
-       output_low(GREEN_LED);
-       delay_ms(500);
-       Error = dma_status(0);
+
+       set_adc_channel(0);
+       Voltage = read_adc();
+       set_adc_channel(2);
+       Temp = read_adc();
+       set_adc_channel(30);
+       probe = read_adc();
+       
+       if((probe < 50) & (input(Probe_Pin == 0)))
+           ProbeId = 0 ; //no probe//
+       if((probe > 200) & (input(Probe_Pin == 1)))
+           ProbeId = 3 ; 
+       if((probe > 200) & (input(Probe_Pin == 0)))
+           ProbeId = 1 ; 
+       if((probe < 50) & (input(Probe_Pin == 1)))
+           ProbeId = 2 ; 
+       
+       TxBuffer[4] = ProbeId;
    }
   
 }   
